aeaca47bd190657e9dc4902a1d904a9056461bf1
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-03-05 09:27:09 +0100

    feat: Added new taglist

--- rc_new.lua
+++ rc_new.lua
@@ -1,10 +1,12 @@
 pcall(require, "luarocks.loader")
 
 local gears = require("gears")
+local gdebug = require("gears.debug")
 local awful = require("awful")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
 local cairo = require("lgi").cairo
+local Rsvg = require('lgi').Rsvg
 local dpi = beautiful.xresources.apply_dpi
 local capi = {
 	drawin = drawin,
@@ -97,22 +99,58 @@ local main_menu = awful.menu({
 })
 
 
+local function float_dpi(size, s)
+	local ratio = s.dpi / 96
+	return size * ratio
+end
+
+
+local function render_svg(path, scaling)
+	local svg = Rsvg.Handle.new_from_file(path)
+	local dim = svg:get_dimensions()
+	local img = cairo.ImageSurface(cairo.Format.ARGB32, dim.width * scaling, dim.height * scaling)
+	local cr = cairo.Context(img)
+	cr:scale(scaling, scaling)
+	svg:render_cairo(cr)
+	return img
+end
+
+
+local taglist_buttons = gears.table.join(
+	awful.button({ }, 1, function(t) t:view_only() end),
+	awful.button({ modkey }, 1, function(t) if client.focus then client.focus:move_to_tag(t) end end),
+	awful.button({ }, 3, awful.tag.viewtoggle),
+	awful.button({ modkey }, 3, function(t) if client.focus then client.focus:toggle_tag(t) end end),
+	awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
+	awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end)
+)
+
+
 for s in screen do
 	gears.wallpaper.maximized(beautiful.wallpaper, s, false)
 end
 
 
---screen.connect_signal("added", function(s)
---	print("added", s)
---end)
+local function setup_screen(s)
+	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+end
+
+awful.screen.connect_for_each_screen(setup_screen)
 --
 --screen.connect_signal("list", function()
 --	print("list signal")
 --end)
 --
+local function set_screen_dpi(s, new_dpi)
+	s.dpi = new_dpi
+	s.tool_bar.height = dpi(18, s)
+	s.taglist:_do_taglist_update_now()
+end
 
 screen.connect_signal("request::desktop_decoration", function(s)
-	s.dpi = 384
+	local scaling = float_dpi(1, s)
+
+	-- main panel
 	s.tool_bar = awful.wibar({
 		position = "top",
 		screen = s,
@@ -143,13 +181,68 @@ screen.connect_signal("request::desktop_decoration", function(s)
 		end
 	})
 
-	local launcher = wibox.widget.imagebox()
-	launcher:set_image(beautiful.launch)
-	launcher:add_button(awful.button({}, 1, nil, function () main_menu:toggle() end))
-	s.launcher = launcher
+	-- launcher as image (because launcher widget don't support svg scaling)
+	s.launcher = wibox.widget.imagebox(beautiful.launch)
+	s.launcher:add_button(awful.button({}, 1, nil, function () main_menu:toggle() end))
+
+	s.taglist_args = {
+		screen = s,
+		filter = awful.widget.taglist.filter.all,
+		buttons = gears.table.join(taglist_buttons),
+		style = {
+			squares_sel = render_svg(beautiful.taglist_squares_sel, scaling),
+			squares_unsel = render_svg(beautiful.taglist_squares_unsel, scaling),
+		},
+		layout = {
+			spacing = 0,
+			layout  = wibox.layout.grid,
+			forced_num_cols = 3,
+			forced_num_rows = 3,
+		},
+		widget_template = {
+			id = "background_role",
+			widget = wibox.container.background,
+			forced_width = dpi(6, s),
+			forced_height = dpi(6, s),
+			{
+				{
+					id = "text_role",
+					widget = wibox.widget.textbox,
+				},
+				{
+					id = "icon_role",
+					widget = wibox.widget.imagebox,
+					forced_width = dpi(6, s),
+					forced_height = dpi(6, s),
+					resize = true,
+				},
+				top = 1000, -- not visible
+				widget = wibox.container.margin,
+			},
+			update_callback = function(self, t, index, objects)
+				local s = t.screen
+				local widgets = self:get_children_by_id('background_role')
+				local size = dpi(6, s)
+				if size ~= widgets[1].forced_width then
+					local scaling = float_dpi(1, s)
+					s.taglist_args.style = {
+						squares_sel = render_svg(beautiful.taglist_squares_sel, scaling),
+						squares_unsel = render_svg(beautiful.taglist_squares_unsel, scaling),
+					}
+					for _, w in ipairs(widgets) do
+						w.forced_width = size
+						w.forced_height = size
+					end
+				end
+			end
+		},
+	}
+
+	s.taglist = awful.widget.taglist(s.taglist_args)
 
 	local left_layout = wibox.layout.fixed.horizontal()
-	left_layout:add(launcher)
+	left_layout:add(s.launcher)
+	left_layout:add(s.taglist)
 
 	s.tool_bar:setup {
 		layout = wibox.layout.align.horizontal,
@@ -160,11 +253,15 @@ end)
 awful.run_test = function()
 	s = screen.fake_add(20, 20, 500, 400)
 	gears.timer {
-		timeout   = 0.05,
+		timeout   = 0.5,
 		call_now  = false,
 		autostart = true,
 		single_shot = true,
 		callback  = function()
+			--set_screen_dpi(s, 384)
+			--for s in screen do
+			--	set_screen_dpi(s, 384)
+			--end
 			s:fake_remove()
 			collectgarbage("collect")
 		end
@@ -196,4 +293,5 @@ awful.run_test = function()
 	--	end
 	--}
 end
+awful.run_test()
 
--- themes/simple-dark/theme_new.lua
+++ themes/simple-dark/theme_new.lua
@@ -3,15 +3,26 @@ theme = {}
 themes_dir = os.getenv("HOME") .. "/.config/awesome/themes/simple-dark"
 
 theme.wallpaper = themes_dir .. "/wall_tmb.jpg"
-theme.wibar_bg = "#2b2e32c0"
-theme.wibar_bg_bottom = "#222528c0"
-theme.wibar_border_bottom = "#111214c0"
-theme.wibar_border_top = "#565c64c0"
+theme.wibar_bg = "#2b2e32f8"
+theme.wibar_bg_bottom = "#222528f8"
+theme.wibar_border_bottom = "#111214f8"
+--theme.wibar_border_top = "#565c64c0"
 
 theme.launch = themes_dir .. "/icons/launch.svg"
 
 theme.menu_height = 20
 theme.menu_width = 140
 theme.menu_submenu_icon = themes_dir .. "/icons/submenu.svg"
+
+theme.taglist_fg_focus                      = '#00000000'
+theme.taglist_fg_urgent                     = '#00000000'
+theme.taglist_fg_occupied                   = '#00000000'
+theme.taglist_fg_empty                      = '#00000000'
+theme.taglist_fg_volatile                   = '#00000000'
+theme.taglist_bg_focus                      = "#186dfe"
+theme.taglist_bg_empty                      = "#00000020"
+theme.taglist_bg_occupied                   = "#00000020"
+theme.taglist_squares_sel                   = themes_dir .. "/icons/square_sel.svg"
+theme.taglist_squares_unsel                   = themes_dir .. "/icons/square_unsel.svg"
 
 return theme
