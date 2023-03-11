12d9c3ee3c904ae8537bc7f4d6a621fa30f280e0
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-03-11 17:15:33 +0100

    feat: Added show_hotkeys_help utility function

--- utils/init.lua
+++ utils/init.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+local gdebug = require("gears.debug")
+local beautiful = require("beautiful")
+local hotkeys_popup = require("awful.hotkeys_popup")
+local awful = require("awful")
+local dpi = beautiful.xresources.apply_dpi
+
 
 M.update_widget_template_attributes = function(widget_template, attributes)
 	for widget_id, attrs in pairs(attributes) do
@@ -15,6 +21,14 @@ M.update_widget_template_attributes = function(widget_template, attributes)
 			M.update_widget_template_attributes(subtemplate, attributes)
 		end
 	end
+end
+
+
+M.show_hotkeys_help = function()
+	local s = awful.screen.focused()
+	beautiful.hotkeys_border_width = dpi(1, s)
+	beautiful.hotkeys_group_margin = dpi(6, s)
+	hotkeys_popup.show_help()
 end
 
 
